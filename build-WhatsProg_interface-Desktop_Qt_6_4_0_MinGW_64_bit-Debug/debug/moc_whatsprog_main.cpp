77a89331f4f42bce4ce01053b2b0fdfd73f476f0
Erika Fernanda <erika.fernanda@smartsuite.imd.ufrn.br>
2022-12-20 23:21:13 -0300

    executa no code blocks

---
+++ build-WhatsProg_interface-Desktop_Qt_6_4_0_MinGW_64_bit-Debug/debug/moc_whatsprog_main.cpp
@@ -0,0 +1,317 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'whatsprog_main.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.4.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../WhatsProg_interface/whatsprog_main.h"
+#include <QtGui/qtextcursor.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'whatsprog_main.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.4.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+namespace {
+struct qt_meta_stringdata_WhatsProgMain_t {
+    uint offsetsAndSizes[54];
+    char stringdata0[14];
+    char stringdata1[10];
+    char stringdata2[1];
+    char stringdata3[12];
+    char stringdata4[21];
+    char stringdata5[31];
+    char stringdata6[32];
+    char stringdata7[37];
+    char stringdata8[24];
+    char stringdata9[28];
+    char stringdata10[12];
+    char stringdata11[6];
+    char stringdata12[26];
+    char stringdata13[34];
+    char stringdata14[33];
+    char stringdata15[36];
+    char stringdata16[36];
+    char stringdata17[13];
+    char stringdata18[3];
+    char stringdata19[6];
+    char stringdata20[6];
+    char stringdata21[12];
+    char stringdata22[20];
+    char stringdata23[20];
+    char stringdata24[23];
+    char stringdata25[12];
+    char stringdata26[2];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_WhatsProgMain_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_WhatsProgMain_t qt_meta_stringdata_WhatsProgMain = {
+    {
+        QT_MOC_LITERAL(0, 13),  // "WhatsProgMain"
+        QT_MOC_LITERAL(14, 9),  // "signLogin"
+        QT_MOC_LITERAL(24, 0),  // ""
+        QT_MOC_LITERAL(25, 11),  // "NovoUsuario"
+        QT_MOC_LITERAL(37, 20),  // "signShowNovaConversa"
+        QT_MOC_LITERAL(58, 30),  // "on_actionDesconectar_triggered"
+        QT_MOC_LITERAL(89, 31),  // "on_actionNovo_usuario_triggered"
+        QT_MOC_LITERAL(121, 36),  // "on_actionUsuario_existente_tr..."
+        QT_MOC_LITERAL(158, 23),  // "on_actionSair_triggered"
+        QT_MOC_LITERAL(182, 27),  // "on_tableConversas_activated"
+        QT_MOC_LITERAL(210, 11),  // "QModelIndex"
+        QT_MOC_LITERAL(222, 5),  // "index"
+        QT_MOC_LITERAL(228, 25),  // "on_tableConversas_clicked"
+        QT_MOC_LITERAL(254, 33),  // "on_lineEditMensagem_returnPre..."
+        QT_MOC_LITERAL(288, 32),  // "on_actionNova_conversa_triggered"
+        QT_MOC_LITERAL(321, 35),  // "on_actionRemover_conversa_tri..."
+        QT_MOC_LITERAL(357, 35),  // "on_actionApagar_mensagens_tri..."
+        QT_MOC_LITERAL(393, 12),  // "slotConectar"
+        QT_MOC_LITERAL(406, 2),  // "IP"
+        QT_MOC_LITERAL(409, 5),  // "login"
+        QT_MOC_LITERAL(415, 5),  // "senha"
+        QT_MOC_LITERAL(421, 11),  // "novoUsuario"
+        QT_MOC_LITERAL(433, 19),  // "slotExibirConversas"
+        QT_MOC_LITERAL(453, 19),  // "slotExibirMensagens"
+        QT_MOC_LITERAL(473, 22),  // "slotExibirErroMensagem"
+        QT_MOC_LITERAL(496, 11),  // "std::string"
+        QT_MOC_LITERAL(508, 1)   // "S"
+    },
+    "WhatsProgMain",
+    "signLogin",
+    "",
+    "NovoUsuario",
+    "signShowNovaConversa",
+    "on_actionDesconectar_triggered",
+    "on_actionNovo_usuario_triggered",
+    "on_actionUsuario_existente_triggered",
+    "on_actionSair_triggered",
+    "on_tableConversas_activated",
+    "QModelIndex",
+    "index",
+    "on_tableConversas_clicked",
+    "on_lineEditMensagem_returnPressed",
+    "on_actionNova_conversa_triggered",
+    "on_actionRemover_conversa_triggered",
+    "on_actionApagar_mensagens_triggered",
+    "slotConectar",
+    "IP",
+    "login",
+    "senha",
+    "novoUsuario",
+    "slotExibirConversas",
+    "slotExibirMensagens",
+    "slotExibirErroMensagem",
+    "std::string",
+    "S"
+};
+#undef QT_MOC_LITERAL
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_WhatsProgMain[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       2,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    1,  110,    2, 0x06,    1 /* Public */,
+       4,    0,  113,    2, 0x06,    3 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       5,    0,  114,    2, 0x08,    4 /* Private */,
+       6,    0,  115,    2, 0x08,    5 /* Private */,
+       7,    0,  116,    2, 0x08,    6 /* Private */,
+       8,    0,  117,    2, 0x08,    7 /* Private */,
+       9,    1,  118,    2, 0x08,    8 /* Private */,
+      12,    1,  121,    2, 0x08,   10 /* Private */,
+      13,    0,  124,    2, 0x08,   12 /* Private */,
+      14,    0,  125,    2, 0x08,   13 /* Private */,
+      15,    0,  126,    2, 0x08,   14 /* Private */,
+      16,    0,  127,    2, 0x08,   15 /* Private */,
+      17,    4,  128,    2, 0x0a,   16 /* Public */,
+      22,    0,  137,    2, 0x0a,   21 /* Public */,
+      23,    0,  138,    2, 0x0a,   22 /* Public */,
+      24,    1,  139,    2, 0x0a,   23 /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Bool,    3,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 10,   11,
+    QMetaType::Void, 0x80000000 | 10,   11,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, QMetaType::QString, QMetaType::QString, QMetaType::Bool,   18,   19,   20,   21,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 25,   26,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject WhatsProgMain::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_WhatsProgMain.offsetsAndSizes,
+    qt_meta_data_WhatsProgMain,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_WhatsProgMain_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<WhatsProgMain, std::true_type>,
+        // method 'signLogin'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'signShowNovaConversa'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionDesconectar_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionNovo_usuario_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionUsuario_existente_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionSair_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_tableConversas_activated'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'on_tableConversas_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QModelIndex &, std::false_type>,
+        // method 'on_lineEditMensagem_returnPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionNova_conversa_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionRemover_conversa_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_actionApagar_mensagens_triggered'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'slotConectar'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const QString &, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'slotExibirConversas'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'slotExibirMensagens'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'slotExibirErroMensagem'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<const std::string, std::false_type>
+    >,
+    nullptr
+} };
+
+void WhatsProgMain::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<WhatsProgMain *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->signLogin((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 1: _t->signShowNovaConversa(); break;
+        case 2: _t->on_actionDesconectar_triggered(); break;
+        case 3: _t->on_actionNovo_usuario_triggered(); break;
+        case 4: _t->on_actionUsuario_existente_triggered(); break;
+        case 5: _t->on_actionSair_triggered(); break;
+        case 6: _t->on_tableConversas_activated((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 7: _t->on_tableConversas_clicked((*reinterpret_cast< std::add_pointer_t<QModelIndex>>(_a[1]))); break;
+        case 8: _t->on_lineEditMensagem_returnPressed(); break;
+        case 9: _t->on_actionNova_conversa_triggered(); break;
+        case 10: _t->on_actionRemover_conversa_triggered(); break;
+        case 11: _t->on_actionApagar_mensagens_triggered(); break;
+        case 12: _t->slotConectar((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[2])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[3])),(*reinterpret_cast< std::add_pointer_t<bool>>(_a[4]))); break;
+        case 13: _t->slotExibirConversas(); break;
+        case 14: _t->slotExibirMensagens(); break;
+        case 15: _t->slotExibirErroMensagem((*reinterpret_cast< std::add_pointer_t<std::string>>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (WhatsProgMain::*)(bool );
+            if (_t _q_method = &WhatsProgMain::signLogin; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (WhatsProgMain::*)();
+            if (_t _q_method = &WhatsProgMain::signShowNovaConversa; *reinterpret_cast<_t *>(_a[1]) == _q_method) {
+                *result = 1;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject *WhatsProgMain::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *WhatsProgMain::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_WhatsProgMain.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int WhatsProgMain::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 16;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void WhatsProgMain::signLogin(bool _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void WhatsProgMain::signShowNovaConversa()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
